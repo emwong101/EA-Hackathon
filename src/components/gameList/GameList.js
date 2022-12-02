a25faee2cf8b6da830c54528fb5bd8d9b91aecf2
emwong101 <emilywonglfa@gmail.com>
2022-12-02 02:57:23 -0800

    added like buttons and fixed layout template

--- src/components/gameList/GameList.js
+++ src/components/gameList/GameList.js
@@ -2,18 +2,15 @@ import React from "react";
 import "./GameList.scss";
 
 export const GameList = ({ img, title, price, discount }) => {
-	return (
-		<section className="game-list">
-			<img
-				src={img}
-				alt="game"
-				className="game-list_img"
-			/>
-			<span className="game-list_title">{title}</span>
-			<div>
-				<span className="game-list_discount">{discount}</span>
-				<span className="game-list_price">{price}</span>
-			</div>
-		</section>
-	);
+  return (
+    <section className="game-list">
+      <img src={img} alt="game" className="game-list_img" />
+      <div className="like"></div>
+      <span className="game-list_title">{title}</span>
+      <div>
+        <span className="game-list_discount">{discount}</span>
+        <span className="game-list_price">{price}</span>
+      </div>
+    </section>
+  );
 };
