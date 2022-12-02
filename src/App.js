23a984f274cca424961908c7a9b4ca1fabfab667
Su Shruthi <shruthacm@gmail.com>
2022-12-01 22:09:21 -0800

    added rightside bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
+import Rightbar from "./components/rightbar/Rightbar";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Rightbar />
     </div>
   );
 }
