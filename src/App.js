2b23182a7bfb072262512e98e309444d63bc8865
emwong101 <emilywonglfa@gmail.com>
2022-12-01 23:34:28 -0800

    finished hero for individual game page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.scss";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
+import SingleGameHero from "./components/singleGameHero/SingleGameHero";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Hero />
+      <SingleGameHero />
     </div>
   );
 }
